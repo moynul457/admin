73f8ce90d5e05494e36c5f4d241bef1b0d0424d9
moynul457 <69568834+moynul457@users.noreply.github.com>
2021-06-22 20:26:25 +0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -52,4 +52,4 @@ else:
 print("you are now hacked")
 print("your all information gethering ")
 print("hackedhack")
-os.system("xdg-open https://www.facebook.com/profile.php?id=100028650051426")
+os.system("xdg-open i")
